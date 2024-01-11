d7740a1ad2f775d561deeb3d502637870474ef91
GrayKS3248 <gschaer2@illinois.edu>
2024-01-11 14:15:02 -0600

    added ae353 wheel project

--- src/condynsate/ae353_wheel.py
+++ src/condynsate/ae353_wheel.py
@@ -1,5 +1,5 @@
 """
-This modules provides a backend for the wheel example
+This modules provides a backend for the ae353 wheel example
 """
 
 ###############################################################################
@@ -18,6 +18,29 @@ class Wheel_sim():
                  visualization_fr=30.,
                  animation=True,
                  animation_fr=15.):
+        """
+        Initializes an instance of the wheel simulation class.
+
+        Parameters
+        ----------
+        visualization : bool, optional
+            A boolean flag that indicates whether the simulation will be 
+            visualized in meshcat. The default is True.
+        visualization_fr : float, optional
+            The frame rate (frames per second) at which the visualizer is
+            updated. The default is 30..
+        animation : bool, optional
+            A boolean flag that indicates whether animated plots are created
+            in real time. The default is True.
+        animation_fr : float, optional
+            The frame rate (frames per second) at which the animated plots are
+            updated. The default is 10..
+
+        Returns
+        -------
+        None.
+
+        """
         # Set the visualization and animation options
         self.visualization = visualization
         self.animation = animation
@@ -82,8 +105,94 @@ class Wheel_sim():
         # Open the animator GUI
         self.sim.open_animator_gui()
 
+
     def run(self,
-            controller):
+            controller,
+            initial_angle = 0.0,
+            initial_target_angle = 3.14159,
+            initial_P = 0.0,
+            initial_D = 0.0):
+        """
+        Runs a complete wheel simulation
+
+        Parameters
+        ----------
+        controller : class
+            A custom class that, at a minimum, must provide the functions:
+                
+                controller.run()
+                Parameters
+                ----------
+                **kwargs
+                
+                Returns
+                -------
+                torque : float
+                The value of torque calculated
+                
+                
+                controller.reset()
+                Parameters
+                ----------
+                None.
+                
+                Returns
+                -------
+                None.
+                
+        initial_angle : Float, optional
+            The initial angle of the wheel in radians. The wheel is set to 
+            this angle when the simulation starts and when the simulation is
+            reset. The default value is 0.0.
+
+        initial_target_angle : Float, optioanl
+            The initial target angle in radians. This is set when the
+            simulation starts and when the simulation is reset. Must be within
+            the range [-pi, pi]. The default value is pi.
+    
+        initial_P : Float, optional
+            The initial value of the proportional gain. This is set when the
+            simulation starts and when the simulation is reset. Must be within
+            the range [0.0, 10.0]. The default value is 0.0.
+    
+        initial_D : Float, optional
+            The initial value of the derivative gain. This is set when the
+            simulation starts and when the simulation is reset. Must be within
+            the range [0.0, 10.0]. The default value is 0.0.
+                
+
+        Returns
+        -------
+        None.
+
+        """
+        # Set the initial values
+        self.sim.set_joint_position(urdf_obj = self.wheel_obj,
+                                    joint_name = "ground_to_axle",
+                                    position = initial_angle,
+                                    physics = False,
+                                    initial_cond = True)
+        self.angle_tag = initial_target_angle
+        self.P = initial_P
+        self.D = initial_D
+        
+        
+        
+        
+        # Await the enter command
+        self.sim.await_keypress(key='enter')
+        
+        # Reset the controller
+        controller.reset()
+        
+        # Create a lists to hold the simulation data
+        time_history = []
+        angle_history =[]
+        target_angle_history = []
+        P_history = []
+        D_history = []
+        torque_history = []
+        
         # Run the simulation loop
         while(not self.sim.is_done):
             ##################################################################
@@ -93,16 +202,26 @@ class Wheel_sim():
             state = self.sim.get_joint_state(urdf_obj=self.wheel_obj,
                                              joint_name="ground_to_axle")
             angle = state['position']
-            angle_vel = state['velocity']
             
             ###################################################################
             # CONTROLLER
             # Get the torque as calculated by the controller
-            torque = controller.get_inputs(target_angle=self.angle_tag,
-                                           angle=angle,
-                                           angle_vel=angle_vel,
-                                           P=self.P,
-                                           D=self.D)
+            inputs = controller.run(target_angle=self.angle_tag,
+                                    angle=angle,
+                                    time=self.sim.time,
+                                    P=self.P,
+                                    D=self.D)
+            torque = inputs[0]
+            
+            ###################################################################
+            # SIMULATION DATA
+            # Append data to history lists
+            time_history.append(self.sim.time)
+            angle_history.append(angle)
+            target_angle_history.append(self.angle_tag)
+            P_history.append(self.P)
+            D_history.append(self.D)
+            torque_history.append(torque)
             
             ###################################################################
             # ACTUATOR
@@ -123,22 +242,22 @@ class Wheel_sim():
                                        x=self.sim.time,
                                        y=angle)
             self.sim.add_subplot_point(subplot_index=self.p1,
-                                       artist_index=self.a2[1],
+                                       artist_index=self.a1[1],
                                        x=self.sim.time,
                                        y=self.angle_tag)
             
             # Plot torque vs time
             self.sim.add_subplot_point(subplot_index=self.p2,
-                                       artist_index=self.a1[0],
+                                       artist_index=self.a2[0],
                                        x=self.sim.time,
                                        y=torque)
             
             # Plot the P and D variables
             self.sim.add_subplot_point(subplot_index=self.p3,
-                                       artist_index=self.a1[0],
+                                       artist_index=self.a3[0],
                                        x=self.P)
             self.sim.add_subplot_point(subplot_index=self.p3,
-                                       artist_index=self.a2[1],
+                                       artist_index=self.a3[1],
                                        x=self.D)
             
             ###################################################################
@@ -177,7 +296,35 @@ class Wheel_sim():
             ###################################################################
             # STEP THE SIMULATION
             # Step the sim
-            self.sim.step(real_time=True,
-                          update_vis=self.visualization,
-                          update_ani=self.animation)
+            val = self.sim.step(real_time=True,
+                                update_vis=self.visualization,
+                                update_ani=self.animation)
+            
+            # Handle resetting the controller and simulation data
+            if val == 3:
+                # Reset the controller
+                controller.reset()
+                
+                # Reset the history
+                time_history = []
+                angle_history =[]
+                target_angle_history = []
+                P_history = []
+                D_history = []
+                torque_history = []
+                
+                # Reset the initial conditions
+                self.angle_tag = initial_target_angle
+                self.P = initial_P
+                self.D = initial_D
+                
+        # When the simulation is done running, gather all simulation data 
+        # into a dictionary and return it
+        data = {'time' : time_history,
+                'angle' : angle_history,
+                'target_angle' : target_angle_history,
+                'P' : P_history,
+                'D' : D_history,
+                'torque' : torque_history}
+        return data
             
