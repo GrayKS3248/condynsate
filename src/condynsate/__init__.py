8eb9db68b70c9e94f3504fcbfcf5009b70e22d85
GrayKS3248 <gschaer2@illinois.edu>
2025-04-17 12:57:00 -0500

    tutorials to .ipynb

--- src/condynsate/__init__.py
+++ src/condynsate/__init__.py
@@ -1,6 +1,23 @@
-from .simulator import Simulator
-from .visualizer import Visualizer
-from .animator import Animator
-from .keyboard import Keys
+# Submodules always needs to be imported to ensure registration
+from .simulator import Simulator # NOQA
+from .visualizer import Visualizer # NOQA
+from .animator import Animator # NOQA
+from .keyboard import Keys # NOQA
 
-__version__ = "0.6.10"
+
+__all__ = ["Simulator",
+           "Visualizer",
+           "Animator",
+           "Keys",]
+
+
+__version__ = '0.6.11'
+
+
+import os
+_root = os.path.split(__file__)[0]
+_dirpath = os.path.join(_root, "__assets__")
+vals = [os.path.join(_dirpath, f) for f in os.listdir(_dirpath)
+        if f.lower().endswith('.urdf')]
+keys = [os.path.split(v)[1][:-5] for v in vals]
+__assets__ = dict(zip(keys, vals))
